b2d343f8f0c14ce3506ddc50780a56b2cb65c461
abhiramS269 <122682964+abhiramS269@users.noreply.github.com>
2023-01-26 19:06:41 +0530

    charector count
    this will print the number of charector repeated in the given string.

---
+++ charector count.py
@@ -0,0 +1,10 @@
+def most_frequent(str):
+    d = dict()
+    for key in str:
+        if key not in d:
+            d[key] = 1
+        else:
+            d[key] += 1
+    return d
+
+print(most_frequent('mississippi'))
